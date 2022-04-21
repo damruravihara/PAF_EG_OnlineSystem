5d3b954ac4607c6c42444c64c66974fc9166810b
“rusiru2000hewageegana@gmail.com” <rusiru2000hewageegana@gmail.com>
2022-04-21 08:32:54 +0530

    create read

--- src/main/java/model/PowerConsumption.java
+++ src/main/java/model/PowerConsumption.java
@@ -2,7 +2,9 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement; 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement; 
 
 public class PowerConsumption {
 	
@@ -58,4 +60,59 @@ public String InsertPowerConsumptionDetails(String customerName, int accountNumb
 	
 	return output;
 	}
+
+public String readPwerConsumption() {
+	
+	String output = "";
+	
+	try {
+		
+		Connection con = connect();
+		
+		if(con == null) {
+			return "Error while connecting to the database for reading.";
+		}
+		
+		 // Prepare the html table to be displayed
+		 output = "<table border='1'><tr><th>Customer Name</th><th>Customer Address</th>" +
+		 "<th>Phone Number</th>" + 
+		 "<th>NIC</th>" +
+		 "<th>UserName</th>" +
+		 "<th>Update</th><th>Remove</th></tr>";
+		 
+		 String query = "select * from customer"; 
+		 Statement stmt = con.createStatement(); 
+		 ResultSet rs = stmt.executeQuery(query); 
+		 
+		 while(rs.next()) {
+			 String accountNumber = Integer.toString(rs.getInt("idcustomer")); 
+			 String customerName = rs.getString("cus_name"); 
+			 String units = rs.getString("cus_address"); 
+			 String days = rs.getString("cus_phone_no"); 
+			
+			 
+			 // Add into the html table
+			 output += "<tr><td>" + accountNumber + "</td>"; 
+			 output += "<td>" + customerName + "</td>"; 
+			 output += "<td>" + units + "</td>"; 
+			 output += "<td>" + days + "</td>";
+			 
+			 
+			 // buttons
+			 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>" + "<td><form method='post' action='customer.jsp'>"
+			 + "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+			 + "<input name='idcustomer' type='hidden' value='" + accountNumber + "'>" + "</form></td></tr>"; 
+		 }
+		 con.close(); 
+		 // Complete the html table
+		 output += "</table>";
+	}
+	catch(Exception e)
+	{
+		 output = "Error while reading the customer."; 
+		 System.err.println(e.getMessage()); 
+	}
+	
+	return output;
+}
 }
