09a8d6f71c3e815d2217a32313945585d989b011
IT20481858 <it20481858@my.sliit.lk>
2022-04-24 12:13:55 +0530

    created payment update

--- src/main/java/com/PaymentService.java
+++ src/main/java/com/PaymentService.java
@@ -4,9 +4,13 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.Payment;
 
@@ -37,4 +41,28 @@ public class PaymentService {
 	 return paymentObj.readPayment(); 
 	}
 
+	
+	@PUT
+	@Path("/update") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updatePayment(String PaymentData) 
+	{ 
+	//Convert the input string to a JSON object 
+	 JsonObject PaymentObject = new JsonParser().parse(PaymentData).getAsJsonObject(); 
+	//Read the values from the JSON object
+	 String PaymentId = PaymentObject.get("PaymentId").getAsString(); 
+	 String AccountNo = PaymentObject.get("AccountNo").getAsString();
+	 String CardNum = PaymentObject.get("CardNum").getAsString(); 
+	 String ExpiryDate= PaymentObject.get("ExpiryDate").getAsString(); 
+	 String CCV = PaymentObject.get("CCV").getAsString(); 
+	 String TelNo = PaymentObject.get("TelNo").getAsString(); 
+	 String output =paymentObj.updatePayment(PaymentId, AccountNo, CardNum, ExpiryDate, CCV, TelNo); 
+	return output; 
+	}
+	
+	
+	
 }
+
+
