e1b794d5df7752430d2ddf296f8f6943f8ff63de
“rusiru2000hewageegana@gmail.com” <rusiru2000hewageegana@gmail.com>
2022-04-24 13:28:10 +0530

    resolve Update errors

--- src/main/java/com/PowerConsumptionService.java
+++ src/main/java/com/PowerConsumptionService.java
@@ -4,9 +4,13 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.PowerConsumption;
 
@@ -40,4 +44,25 @@ public class PowerConsumptionService {
 		 String output = consumeOb.InsertPowerConsumptionDetails(userID,account_Number,cus_name,units,days,generated_date);
 		 return output; 
 	 }
+	 
+	 @PUT
+	 @Path("/")
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String updateConsumption(String updateCondumptionDetails)
+	 {
+		 JsonObject updateCons = new JsonParser().parse(updateCondumptionDetails).getAsJsonObject();
+		 
+		 String userID = updateCons.get("userID").getAsString();
+		 String account_Number = updateCons.get("account_Number").getAsString();
+		 String cus_name = updateCons.get("cus_name").getAsString();
+		 String units = updateCons.get("units").getAsString();
+		 String days = updateCons.get("days").getAsString();
+		 String generated_date = updateCons.get("generated_date").getAsString();
+		 String idpower_consumption = updateCons.get("idpower_consumption").getAsString();
+		 
+		 String output = consumeOb.updateConsumption(idpower_consumption, userID, account_Number, cus_name, units, days, generated_date);
+		 return output;
+	 }
+	 
 }
